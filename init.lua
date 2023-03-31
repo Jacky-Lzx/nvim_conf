b90cea4e747c558f1fa08452814dce53c8c76885
Zexi. Li <lzx12138@sjtu.edu.cn>
2023-03-31 16:24:06 +0800

    small: changed nvim-tree configs.

--- init.lua
+++ init.lua
@@ -1,8 +1,10 @@
+--- From nvim-tree
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
+--- End
 
 vim.opt.number = true
 vim.opt.ignorecase = true
