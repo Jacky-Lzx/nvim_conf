31708bdb384fb5aa82b1cc63c57a70caaf92db99
Zexi. Li <lzx12138@sjtu.edu.cn>
2023-04-03 19:56:23 +0800

    bug: move config back to the end of the file.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,17 +22,15 @@ require("packer").startup({
 		use({
 			"phaazon/hop.nvim",
 			branch = "v2", -- optional but strongly recommended
-			config = require("plugin-settings/hop"),
 		})
 		use({
 			"ggandor/leap.nvim",
 			requires = { "tpope/vim-repeat" },
-			config = require("plugin-settings/leap"),
 		})
 
-		use({ "ellisonleao/gruvbox.nvim", config = require("plugin-settings/gruvbox") })
+		use({ "ellisonleao/gruvbox.nvim" })
 
-		use({ "numToStr/Comment.nvim", config = require("plugin-settings/comment") })
+		use({ "numToStr/Comment.nvim" })
 
 		use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 		use("nvim-telescope/telescope-ui-select.nvim")
@@ -49,7 +47,6 @@ require("packer").startup({
 					-- the default case_mode is "smart_case"
 				},
 			},
-			config = require("plugin-settings.telescope"),
 		})
 
 		use({
@@ -58,63 +55,40 @@ require("packer").startup({
 				"nvim-tree/nvim-web-devicons", -- optional, for file icons
 			},
 			tag = "nightly", -- optional, updated every week. (see issue #1193)
-			config = require("plugin-settings/nvim-tree"),
 		})
 
-		use({ "numToStr/FTerm.nvim", config = require("plugin-settings.fterm") })
+		use({ "numToStr/FTerm.nvim" })
 
 		use({
 			"cappyzawa/trim.nvim",
-			config = require("trim").setup({
-				-- if you want to ignore markdown file.
-				-- you can specify filetypes.
-				-- disable = {"markdown"},
-
-				-- if you want to remove multiple blank lines
-				-- patterns = {
-				--   [[%s/\(\n\n\)\n\+/\1/]],   -- replace multiple blank lines with a single line
-				-- },
-				--
-				trim_trailing = true,
-				trim_last_line = true,
-				trim_first_line = false,
-			}),
 		})
 
-		use({ "norcalli/nvim-colorizer.lua", config = require("colorizer").setup() })
+		use({ "norcalli/nvim-colorizer.lua" })
 
 		-- The command of Buffer delete
 		use("moll/vim-bbye")
 		use({
 			"nvim-lualine/lualine.nvim",
 			requires = { "kyazdani42/nvim-web-devicons", opt = true },
-			config = require("plugin-settings.lualine"),
 		})
 		use({
 			"kdheepak/tabline.nvim",
 			requires = { { "hoob3rt/lualine.nvim", opt = true }, { "kyazdani42/nvim-web-devicons", opt = true } },
-			config = require("plugin-settings.tabline"),
 		})
 
-		use({ "mbbill/undotree", config = require("plugin-settings/undotree") })
+		use({ "mbbill/undotree" })
 
 		use("dstein64/vim-startuptime")
 
 		use({
 			"lukas-reineke/indent-blankline.nvim",
-			config = require("indent_blankline").setup({
-				space_char_blankline = " ",
-				show_current_context = true,
-				show_current_context_start = true,
-			}),
 		})
 
-		use({ "folke/which-key.nvim", config = require("which-key").setup() })
+		use({ "folke/which-key.nvim" })
 
 		use({
 			"goolord/alpha-nvim",
 			requires = { "nvim-tree/nvim-web-devicons" },
-			cofig = require("alpha").setup(require("alpha.themes.startify").config),
 		})
 
 		use("lewis6991/gitsigns.nvim")
@@ -123,14 +97,13 @@ require("packer").startup({
 
 		-- use({ "edluffy/specs.nvim", config = require("plugin-settings.specs") })
 
-		use({ "mhartington/formatter.nvim", config = require("plugin-settings/formatter") })
+		use({ "mhartington/formatter.nvim" })
 
 		use("mg979/vim-visual-multi")
 
 		use({
 			"jose-elias-alvarez/null-ls.nvim",
 			requires = { "nvim-lua/plenary.nvim" },
-			config = require("plugin-settings/null-ls"),
 		})
 
 		use({
@@ -140,7 +113,6 @@ require("packer").startup({
 				local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
 				ts_update()
 			end,
-			config = require("plugin-settings/nvim-treesitter"),
 		})
 
 		use({
@@ -149,7 +121,7 @@ require("packer").startup({
 			"williamboman/mason-lspconfig.nvim",
 		})
 
-		use({ "windwp/nvim-autopairs", config = require("nvim-autopairs").setup() })
+		use({ "windwp/nvim-autopairs" })
 		use("hrsh7th/cmp-nvim-lsp") -- { name = nvim_lsp }
 		use("hrsh7th/cmp-buffer") -- { name = 'buffer' },
 		-- use("hrsh7th/cmp-path") -- { name = 'path' }
@@ -167,31 +139,24 @@ require("packer").startup({
 			tag = "v1.*",
 			-- install jsregexp (optional!:).
 			run = "make install_jsregexp",
-			config = require("plugin-settings/luasnip"),
 		})
 		use("saadparwaiz1/cmp_luasnip")
 		use("rafamadriz/friendly-snippets")
 		-- lspkind
 		use("onsails/lspkind-nvim")
 
-		use({ "lewis6991/hover.nvim", config = require("plugin-settings/hover") })
+		use({ "lewis6991/hover.nvim" })
 
 		-- use({ "stevearc/aerial.nvim", config = require("aerial").setup() })
-		use({ "lervag/vimtex", config = require("plugin-settings/vimtex") })
+		use({ "lervag/vimtex" })
 
 		use({
 			"kylechui/nvim-surround",
 			tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-			config = function()
-				require("nvim-surround").setup({
-					-- Configuration here, or leave empty to use defaults
-				})
-			end,
 		})
 
 		use({
 			"folke/noice.nvim",
-			config = require("plugin-settings.noice"),
 			requires = {
 				-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 				"MunifTanjim/nui.nvim",
@@ -204,31 +169,6 @@ require("packer").startup({
 
 		use({
 			"ibhagwan/smartyank.nvim",
-			config = {
-				require("smartyank").setup({
-					highlight = {
-						enabled = true, -- highlight yanked text
-						higroup = "IncSearch", -- highlight group of yanked text
-						timeout = 400, -- timeout for clearing the highlight
-					},
-					clipboard = {
-						enabled = true,
-					},
-					tmux = {
-						enabled = true,
-						-- remove `-w` to disable copy to host client's clipboard
-						cmd = { "tmux", "set-buffer", "-w" },
-					},
-					osc52 = {
-						enabled = true,
-						-- escseq = 'tmux',     -- use tmux escape sequence, only enable if
-						-- you're using tmux and have issues (see #4)
-						ssh_only = true, -- false to OSC52 yank also in local sessions
-						silent = false, -- true to disable the "n chars copied" echo
-						echo_hl = "Directory", -- highlight group of the OSC52 echo message
-					},
-				}),
-			},
 		})
 
 		if packer_bootstrap then
@@ -251,6 +191,7 @@ require("plugin-settings.mason")
 require("gitsigns").setup()
 -- require("scrollbar.handlers.search").setup({ nearest_only = true })
 require("scrollbar.handlers.gitsigns").setup()
+
 require("scrollbar").setup({
 	handle = {
 		text = " ",
@@ -272,3 +213,73 @@ require("scrollbar").setup({
 -- })
 
 require("plugin-settings.cmp")
+require("plugin-settings/hop")
+require("plugin-settings/leap")
+require("plugin-settings/gruvbox")
+require("plugin-settings/comment")
+require("plugin-settings.telescope")
+require("plugin-settings/nvim-tree")
+require("plugin-settings.fterm")
+
+require("trim").setup({
+	-- if you want to ignore markdown file.
+	-- you can specify filetypes.
+	-- disable = {"markdown"},
+
+	-- if you want to remove multiple blank lines
+	-- patterns = {
+	--   [[%s/\(\n\n\)\n\+/\1/]],   -- replace multiple blank lines with a single line
+	-- },
+	--
+	trim_trailing = true,
+	trim_last_line = true,
+	trim_first_line = false,
+})
+require("colorizer").setup()
+require("plugin-settings.lualine")
+require("plugin-settings.tabline")
+require("plugin-settings/undotree")
+
+require("indent_blankline").setup({
+	space_char_blankline = " ",
+	show_current_context = true,
+	show_current_context_start = true,
+})
+require("which-key").setup()
+require("plugin-settings/formatter")
+require("plugin-settings/null-ls")
+require("plugin-settings/nvim-treesitter")
+require("nvim-autopairs").setup()
+require("plugin-settings/luasnip")
+require("plugin-settings/hover")
+require("plugin-settings/vimtex")
+
+require("nvim-surround").setup({
+	-- Configuration here, or leave empty to use defaults
+})
+require("plugin-settings.noice")
+
+require("smartyank").setup({
+	highlight = {
+		enabled = true, -- highlight yanked text
+		higroup = "IncSearch", -- highlight group of yanked text
+		timeout = 400, -- timeout for clearing the highlight
+	},
+	clipboard = {
+		enabled = true,
+	},
+	tmux = {
+		enabled = true,
+		-- remove `-w` to disable copy to host client's clipboard
+		cmd = { "tmux", "set-buffer", "-w" },
+	},
+	osc52 = {
+		enabled = true,
+		-- escseq = 'tmux',     -- use tmux escape sequence, only enable if
+		-- you're using tmux and have issues (see #4)
+		ssh_only = true, -- false to OSC52 yank also in local sessions
+		silent = false, -- true to disable the "n chars copied" echo
+		echo_hl = "Directory", -- highlight group of the OSC52 echo message
+	},
+})
+require("alpha").setup(require("alpha.themes.startify").config)
