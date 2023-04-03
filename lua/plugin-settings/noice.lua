f36dc5fdababf0b9be2e98c2562ab9052ad8ab34
Zexi. Li <lzx12138@sjtu.edu.cn>
2023-04-03 19:29:34 +0800

    small: changed noice configs.

--- lua/plugin-settings/noice.lua
+++ lua/plugin-settings/noice.lua
@@ -1,11 +1,32 @@
+require("notify").setup({
+	background_colour = "#282828",
+})
+
 require("noice").setup({
 	-- add any options here
 	lsp = {
+		progress = {
+			enabled = true,
+			-- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+			-- See the section on formatting for more details on how to customize.
+			--- @type NoiceFormat|string
+			format = "lsp_progress",
+			--- @type NoiceFormat|string
+			format_done = "lsp_progress_done",
+			throttle = 1000 / 30, -- frequency to update lsp progress message
+			view = "mini",
+		},
 		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			["vim.lsp.util.stylize_markdown"] = true,
 			["cmp.entry.get_documentation"] = true,
+		},
+		message = {
+			-- Messages shown by lsp servers
+			enabled = true,
+			view = "notify",
+			opts = {},
 		},
 	},
 	-- you can enable a preset for easier configuration
@@ -19,9 +40,6 @@ require("noice").setup({
 })
 
 require("telescope").load_extension("noice")
-require("notify").setup({
-	background_colour = "#282828",
-})
 
 vim.keymap.set("n", "<leader>nl", function()
 	require("noice").cmd("last")
