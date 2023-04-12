4584099dee8c39310e1afded034d2d4433ae2050
Zexi. Li <lzx12138@sjtu.edu.cn>
2023-04-12 20:47:22 +0800

    change: changed gruvbox settings.

--- lua/plugins_new/ui.lua
+++ lua/plugins_new/ui.lua
@@ -59,35 +59,33 @@ return {
   {
     "ellisonleao/gruvbox.nvim",
     lazy = false,
-    config = function()
-      local opts = {
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        -- transparent_mode = false,
-        transparent_mode = true,
-      }
-      -- require("gruvbox").setup({italic = {comments = false}})
+    opts = {
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      -- transparent_mode = false,
+      transparent_mode = true,
+    },
+    config = function(_, opts)
       require("gruvbox").setup(opts)
 
       vim.o.background = "dark" -- or "light" for light mode
-      -- vim.cmd([[colorscheme gruvbox]])
       vim.cmd([[colorscheme gruvbox]])
       vim.cmd([[colorscheme gruvbox]])
     end,
