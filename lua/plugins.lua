1e3398631db67e66f70a13b0f5d16e39065f1c65
Zexi. Li <lzx12138@sjtu.edu.cn>
2023-04-12 19:17:52 +0800

    small: changed Lazy nvim ui setting.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -45,28 +45,6 @@ local opts = {
         "‒",
       },
     },
-    -- leave nil, to automatically select a browser depending on your OS.
-    -- If you want to use a specific browser, you can define it here
-    browser = nil, ---@type string?
-    throttle = 20, -- how frequently should the ui process render events
-    custom_keys = {
-      -- you can define custom key maps here.
-      -- To disable one of the defaults, set it to false
-
-      -- open lazygit log
-      ["<localleader>l"] = function(plugin)
-        require("lazy.util").float_term({ "lazygit", "log" }, {
-          cwd = plugin.dir,
-        })
-      end,
-
-      -- open a terminal for the plugin dir
-      ["<localleader>t"] = function(plugin)
-        require("lazy.util").float_term(nil, {
-          cwd = plugin.dir,
-        })
-      end,
-    },
   },
 }
 
