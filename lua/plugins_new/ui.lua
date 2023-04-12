f49549dce5fd9973a8499a63b020fd50bf36db55
Zexi. Li <lzx12138@sjtu.edu.cn>
2023-04-12 20:38:09 +0800

    change: changed noice settings.

--- lua/plugins_new/ui.lua
+++ lua/plugins_new/ui.lua
@@ -264,58 +264,85 @@ return {
 
   {
     "folke/noice.nvim",
-    config = function()
-      -- require("noice").setup({
-      --   -- add any options here
-      -- })
-      require("notify").setup({
-        background_colour = "#282828",
-      })
-
-      require("noice").setup({
-        -- add any options here
-        lsp = {
-          progress = {
-            enabled = true,
-            -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
-            -- See the section on formatting for more details on how to customize.
-            --- @type NoiceFormat|string
-            format = "lsp_progress",
-            --- @type NoiceFormat|string
-            format_done = "lsp_progress_done",
-            throttle = 1000 / 30, -- frequency to update lsp progress message
-            view = "mini",
+    lazy = false,
+    keys = {
+      {
+        "<leader>nl",
+        function()
+          require("noice").cmd("last")
+        end,
+        desc = "noice last message",
+      },
+      {
+        "<leader>nh",
+        function()
+          require("noice").cmd("history")
+        end,
+        desc = "noice history",
+      },
+    },
+    opts = {
+      notify = {
+        -- Noice can be used as `vim.notify` so you can route any notification like other messages
+        -- Notification messages have their level and other properties set.
+        -- event is always "notify" and kind can be any log level as a string
+        -- The default routes will forward notifications to nvim-notify
+        -- Benefit of using Noice for this is the routing and consistent history view
+        enabled = true,
+        view = "notify",
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            kind = "search_count",
           },
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
-          message = {
-            -- Messages shown by lsp servers
-            enabled = true,
-            view = "notify",
-            opts = {},
-          },
+          opts = { skip = true },
         },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+          },
+          opts = { skip = true },
         },
-      })
-
-      vim.keymap.set("n", "<leader>nl", function()
-        require("noice").cmd("last")
-      end)
-
-      vim.keymap.set("n", "<leader>nh", function()
-        require("noice").cmd("history")
-      end)
+      },
+      lsp = {
+        progress = {
+          enabled = true,
+          -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+          -- See the section on formatting for more details on how to customize.
+          --- @type NoiceFormat|string
+          format = "lsp_progress",
+          --- @type NoiceFormat|string
+          format_done = "lsp_progress_done",
+          throttle = 1000 / 30, -- frequency to update lsp progress message
+          view = "mini",
+        },
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+        message = {
+          -- Messages shown by lsp servers
+          enabled = true,
+          view = "notify",
+          opts = {},
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    },
+    config = function(_, opts)
+      require("noice").setup(opts)
     end,
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -323,7 +350,7 @@ return {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
+      { "rcarriga/nvim-notify", opts = { background_colour = "#282828" } },
     },
   },
 }
