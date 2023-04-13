892a59ab035f7411988d4bf7bb90add699095333
Zexi. Li <lzx12138@sjtu.edu.cn>
2023-04-13 14:38:25 +0800

    fix: fixed bug about telescoope themes.

--- lua/plugins_new/ui.lua
+++ lua/plugins_new/ui.lua
@@ -158,19 +158,21 @@ return {
     },
     lazy = true,
     cmd = "Telescope",
-    opts = {
-      extensions = {
-        ["fzf"] = {
-          fuzzy = true, -- false will only do exact matching
-          override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true, -- override the file sorter
-          case_mode = "smart_case", -- or "ignore_case" or "respect_case", the default case_mode is "smart_case"
+    opts = function()
+      return {
+        extensions = {
+          ["fzf"] = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case", the default case_mode is "smart_case"
+          },
+          ["ui-select"] = {
+            require("telescope.themes").get_cursor(),
+          },
         },
-        ["ui-select"] = {
-          require("telescope.themes").get_cursor(),
-        },
-      },
-    },
+      }
+    end,
     keys = {
       { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "buffers" },
       { "<leader>fd", "<cmd>Telescope lsp_definitions<cr>", desc = "lsp definitions" },
