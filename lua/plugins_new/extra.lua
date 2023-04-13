2aa681627b05f49b97952225b4caef54fbb22fe0
Zexi. Li <lzx12138@sjtu.edu.cn>
2023-04-13 12:39:09 +0800

    change: changed hop and leap settings.

--- lua/plugins_new/extra.lua
+++ lua/plugins_new/extra.lua
@@ -82,59 +82,39 @@ return {
   -- use 'easymotion/vim-easymotion'
   {
     "phaazon/hop.nvim",
+    lazy = true,
     branch = "v2", -- optional but strongly recommended
-    config = function()
-      require("hop").setup()
-      local hop = require("hop")
-
-      -- local directions = require("hop.hint").HintDirection
-
-      -- vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
-      --   hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-      -- end, { remap = true })
-
-      -- vim.keymap.set({ 'n', 'v' }, '<leader>s', function()
-      --   -- hop.hint_char2({current_line_only = false})
-      --   hop.hint_char2()
-      -- end, { remap = true })
-
-      -- :HopLine
-      -- vim.keymap.set({ "n", "v" }, "<leader>j", function()
-      --   hop.hint_lines({ direction = directions.AFTER_CURSOR, current_line_only = false })
-      -- end, { remap = true })
-
-      -- vim.keymap.set({ "n", "v" }, "<leader>k", function()
-      --   hop.hint_lines({ direction = directions.BEFORE_CURSOR, current_line_only = false })
-      -- end, { remap = true })
-
-      vim.keymap.set({ "n", "v" }, "<leader>j", function()
-        hop.hint_lines({ current_line_only = false })
-      end, { remap = true })
-
-      vim.keymap.set({ "n", "v" }, "<leader>k", function()
-        hop.hint_lines({ current_line_only = false })
-      end, { remap = true })
+    keys = {
+      {
+        "<leader>j",
+        function()
+          require("hop").hint_lines({ current_line_only = false })
+        end,
+        mode = { "n", "v" },
+        desc = "hop jump",
+      },
+      {
+        "<leader>k",
+        function()
+          require("hop").hint_lines({ current_line_only = false })
+        end,
+        mode = { "n", "v" },
+        desc = "hop jump",
+      },
+    },
+    opts = {},
+    config = function(_, opts)
+      require("hop").setup(opts)
     end,
   },
   {
     "ggandor/leap.nvim",
     dependencies = { "tpope/vim-repeat" },
-    config = function()
-      require("leap").add_default_mappings()
-
-      vim.keymap.del({ "n", "x", "o" }, "s")
-      vim.keymap.del({ "n", "x", "o" }, "S")
-      vim.keymap.del({ "x", "o" }, "x")
-      vim.keymap.del({ "x", "o" }, "X")
-
-      vim.keymap.set({ "n", "x", "o" }, "<leader>s", "<Plug>(leap-forward-to)")
-      vim.keymap.set({ "n", "x", "o" }, "<leader>S", "<Plug>(leap-backward-to)")
-
-      -- vim.keymap.set({ "n", "x", "o" }, "<leader>x", "<Plug>(leap-forward-till)")
-      -- vim.keymap.set({ "n", "x", "o" }, "<leader>X", "<Plug>(leap-backward-till)")
-
-      vim.keymap.set({ "n", "x", "o" }, "<leader>gs", "<Plug>(leap-from-window)")
-    end,
+    keys = {
+      { "<leader>s", "<Plug>(leap-forward-to)", mode = { "n", "x", "o" }, desc = "leap jump forward" },
+      { "<leader>S", "<Plug>(leap-backward-to)", mode = { "n", "x", "o" }, desc = "leap jump backward" },
+      { "<leader>gs", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "leap jump window" },
+    },
   },
 
   {
