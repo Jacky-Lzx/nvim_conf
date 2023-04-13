0e68846ea35a8010fe8c24aa4f67055266738735
Zexi. Li <lzx12138@sjtu.edu.cn>
2023-04-13 13:43:19 +0800

    feat: added two ui plugins.

--- lua/plugins_new/ui.lua
+++ lua/plugins_new/ui.lua
@@ -322,4 +322,21 @@ return {
       { "rcarriga/nvim-notify", opts = { background_colour = "#282828" } },
     },
   },
+
+  {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+  },
 }
