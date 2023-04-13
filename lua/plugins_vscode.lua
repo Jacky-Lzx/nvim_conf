efb36b054b150c97ae58c41b1d883a63be48161b
Zexi. Li <lzx12138@sjtu.edu.cn>
2023-04-13 12:49:22 +0800

    change: changed vscode settings.

--- lua/plugins_vscode.lua
+++ lua/plugins_vscode.lua
@@ -1,78 +1,121 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system {
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
-		lazypath,
-	}
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 plugins = {}
 opts = {
-	git = {
-		clone_timeout = 600,
-		url_format = "git@github.com:%s.git",
-	},
+  git = {
+    clone_timeout = 600,
+    url_format = "git@github.com:%s.git",
+  },
 }
 
--- require("lazy").setup(plugins, opts)
 require("lazy").setup({
-	-- use 'easymotion/vim-easymotion'
-	{
-		"phaazon/hop.nvim",
-		branch = "v2", -- optional but strongly recommended
-	},
-	{
-		"ggandor/leap.nvim",
-		dependencies = { "tpope/vim-repeat" },
-	},
-	{
-		"kylechui/nvim-surround",
-		-- version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	},
-	{ "numToStr/Comment.nvim" },
-	{
-		"cappyzawa/trim.nvim",
-	},
-	{
-		"ibhagwan/smartyank.nvim",
-	},
+  {
+    "phaazon/hop.nvim",
+    lazy = true,
+    branch = "v2", -- optional but strongly recommended
+    keys = {
+      {
+        "<leader>j",
+        function()
+          require("hop").hint_lines({ current_line_only = false })
+        end,
+        mode = { "n", "v" },
+        desc = "hop jump",
+      },
+      {
+        "<leader>k",
+        function()
+          require("hop").hint_lines({ current_line_only = false })
+        end,
+        mode = { "n", "v" },
+        desc = "hop jump",
+      },
+    },
+    opts = {},
+    config = function(_, opts)
+      require("hop").setup(opts)
+    end,
+  },
+  {
+    "ggandor/leap.nvim",
+    dependencies = { "tpope/vim-repeat" },
+    keys = {
+      { "<leader>s", "<Plug>(leap-forward-to)", mode = { "n", "x", "o" }, desc = "leap jump forward" },
+      { "<leader>S", "<Plug>(leap-backward-to)", mode = { "n", "x", "o" }, desc = "leap jump backward" },
+      { "<leader>gs", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "leap jump window" },
+    },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    keys = {
+      {
+        "<leader>/",
+        function()
+          require("Comment.api").toggle.linewise.current()
+        end,
+        desc = "comment current line",
+      },
+      {
+        "<leader>/",
+        "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+        mode = "v",
+        desc = "comment current line",
+      },
+    },
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+  {
+    "cappyzawa/trim.nvim",
+    config = function()
+      require("trim").setup()
+    end,
+  },
+  {
+    "ibhagwan/smartyank.nvim",
+    opts = {
+      highlight = {
+        enabled = true, -- highlight yanked text
+        higroup = "IncSearch", -- highlight group of yanked text
+        timeout = 400, -- timeout for clearing the highlight
+      },
+      clipboard = {
+        enabled = true,
+      },
+      tmux = {
+        enabled = true,
+        -- remove `-w` to disable copy to host client's clipboard
+        cmd = { "tmux", "set-buffer", "-w" },
+      },
+      osc52 = {
+        enabled = true,
+        -- escseq = 'tmux',     -- use tmux escape sequence, only enable if
+        -- you're using tmux and have issues (see #4)
+        ssh_only = true, -- false to OSC52 yank also in local sessions
+        silent = false, -- true to disable the "n chars copied" echo
+        echo_hl = "Directory", -- highlight group of the OSC52 echo message
+      },
+    },
+  },
 }, opts)
-
-require("plugin-settings/hop")
-require("plugin-settings/leap")
-require("plugin-settings/comment")
-
-require("nvim-surround").setup({
-	-- Configuration here, or leave empty to use defaults
-})
-
-require("smartyank").setup({
-	highlight = {
-		enabled = true,      -- highlight yanked text
-		higroup = "IncSearch", -- highlight group of yanked text
-		timeout = 400,       -- timeout for clearing the highlight
-	},
-	clipboard = {
-		enabled = true,
-	},
-	tmux = {
-		enabled = true,
-		-- remove `-w` to disable copy to host client's clipboard
-		cmd = { "tmux", "set-buffer", "-w" },
-	},
-	osc52 = {
-		enabled = true,
-		-- escseq = 'tmux',     -- use tmux escape sequence, only enable if
-		-- you're using tmux and have issues (see #4)
-		ssh_only = true,     -- false to OSC52 yank also in local sessions
-		silent = false,      -- true to disable the "n chars copied" echo
-		echo_hl = "Directory", -- highlight group of the OSC52 echo message
-	},
-})
-
-require("trim").setup()
