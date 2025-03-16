2ff97a7d4ba8aef6637ed1192e37ffdff9c46f35
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-03-16 12:57:41 +0800

    feat(CopilotChat): added copilot chat plugin.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,6 +7,28 @@ end
 return {
 
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      -- { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      mappings = {
+        submit_prompt = {
+          normal = "<Leader>s",
+          insert = "<C-s>",
+        },
+        show_diff = {
+          -- full_diff = true,
+        },
+      },
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+  {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
