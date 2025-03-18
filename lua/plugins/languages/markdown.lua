4e6b7e884dbb19fd077b34fbe243a6cd799acaf0
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-03-18 20:28:28 +0800

    feat(markdown): added a new plugin for markdown preview in browser.

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -20,6 +20,15 @@ end
 
 M.plugins = {
   {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+  {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = "markdown",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons", "hrsh7th/nvim-cmp" }, -- if you prefer nvim-web-devicons
