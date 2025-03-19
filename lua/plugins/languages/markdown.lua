3e03080298db7111157e52763d9df151f53fbf53
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-03-20 00:04:25 +0800

    small(markdown): disabled obsidian plugin, waiting it to support blink.cmp.

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -89,32 +89,32 @@ M.plugins = {
   },
 
   -- Obsidian Plugin
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    event = {
-      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-      -- refer to `:h file-pattern` for more examples
-      "BufReadPre ~/Documents_lzx/Obsidian/Research/*.md",
-      "BufNewFile ~/Documents_lzx/Obsidian/Research/*.md",
-    },
-    dependencies = {
-      -- Required.
-      "nvim-lua/plenary.nvim",
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "research",
-          path = "~/Documents_lzx/Obsidian/Research",
-        },
-      },
-    },
-  },
+  -- {
+  --   "epwalsh/obsidian.nvim",
+  --   version = "*", -- recommended, use latest release instead of latest commit
+  --   lazy = true,
+  --   ft = "markdown",
+  --   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  --   event = {
+  --     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --     -- refer to `:h file-pattern` for more examples
+  --     "BufReadPre ~/Documents_lzx/Obsidian/Research/*.md",
+  --     "BufNewFile ~/Documents_lzx/Obsidian/Research/*.md",
+  --   },
+  --   dependencies = {
+  --     -- Required.
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   opts = {
+  --     workspaces = {
+  --       {
+  --         name = "research",
+  --         path = "~/Documents_lzx/Obsidian/Research",
+  --       },
+  --     },
+  --   },
+  -- },
 }
 
 return M
