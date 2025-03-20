dc35f512ddb4c337b87ea529d5ff4e32774f744c
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-03-20 21:34:33 +0800

    style(ai): organized ai.lua.

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -10,10 +10,10 @@ return {
 
     -- stylua: ignore
     keys = {
-      {"<leader>cca", "<CMD>CodeCompanionActions<CR>", mode = {"n", "v"}, noremap = true, silent = true, desc = "CodeCompanion actions"},
-      {"<leader>cci", "<CMD>CodeCompanion<CR>", mode = {"n", "v"}, noremap = true, silent = true, desc = "CodeCompanion inline"},
+      {"<leader>cca", "<CMD>CodeCompanionActions<CR>",     mode = {"n", "v"}, noremap = true, silent = true, desc = "CodeCompanion actions"      },
+      {"<leader>cci", "<CMD>CodeCompanion<CR>",            mode = {"n", "v"}, noremap = true, silent = true, desc = "CodeCompanion inline"       },
       {"<leader>ccc", "<CMD>CodeCompanionChat Toggle<CR>", mode = {"n", "v"}, noremap = true, silent = true, desc = "CodeCompanion chat (toggle)"},
-      {"<leader>ccp", "<CMD>CodeCompanionChat Add<CR>", mode = { "v"}, noremap = true, silent = true, desc = "CodeCompanion chat add code"},
+      {"<leader>ccp", "<CMD>CodeCompanionChat Add<CR>",    mode = {"v"}     , noremap = true, silent = true, desc = "CodeCompanion chat add code"},
     },
 
     opts = {
@@ -21,13 +21,12 @@ return {
 
       display = {
         chat = {
-          -- If show settings, can not change adapter during the chat
-          show_settings = false,
+          show_settings = false, -- If show settings, can not change adapter during the chat
         },
         diff = {
-          layout = "vertical", -- vertical|horizontal split for default provider
+          layout = "vertical", -- "vertical"|"horizontal" split for default provider
           opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
-          provider = "mini_diff", -- default|mini_diff
+          provider = "mini_diff", -- "default"|"mini_diff"
         },
       },
 
@@ -43,68 +42,26 @@ return {
         end,
       },
       strategies = {
-        chat = {
-          adapter = "copilot",
-        },
-        inline = {
-          adapter = "copilot",
-        },
+        chat = { adapter = "copilot" },
+        inline = { adapter = "copilot" },
       },
 
       opts = {
-        language = "English",
-        -- language = "Chinese",
+        language = "English", -- "English"|"Chinese"
       },
     },
-
-    config = function(_, opts)
-      require("codecompanion").setup(opts)
-
-      -- Expand 'cc' into 'CodeCompanion' in the command line
-      -- vim.cmd([[cab cc CodeCompanion]])
-    end,
   },
-
-  -- {
-  --   "CopilotC-Nvim/CopilotChat.nvim",
-  --   dependencies = {
-  --     -- { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-  --     { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
-  --     { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-  --   },
-  --   build = "make tiktoken", -- Only on MacOS or Linux
-  --   cmd = { "CopilotChat", "CopilotChatToggle", "CopilotChatPrompts" },
-  --   opts = {
-  --     mappings = {
-  --       submit_prompt = {
-  --         normal = "<Leader>s",
-  --         insert = "<C-s>",
-  --       },
-  --       show_diff = {
-  --         -- full_diff = true,
-  --       },
-  --     },
-  --     -- See Configuration section for options
-  --   },
-  --   -- See Commands section for default commands if you want to lazy load on them
-  -- },
 
   {
     "zbirenbaum/copilot.lua",
-
-    -- dependencies = {
-    --   {
-    --     "zbirenbaum/copilot-cmp",
-    --     config = function()
-    --       require("copilot_cmp").setup()
-    --     end,
-    --   },
-    -- },
-    -- cmd = "Copilot",
     event = "VeryLazy",
     opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
+      suggestion = {
+        enabled = false,
+      },
+      panel = {
+        enabled = false,
+      },
       filetypes = {
         markdown = true,
         help = true,
