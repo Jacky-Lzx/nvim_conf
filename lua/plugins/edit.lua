ed2fce4a43bfd8f49ceb0d3bdbecc43a6e407f0a
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-03-30 01:00:33 +0800

    fix(comment): fixed the comment keymapping issue intrduced in a551b47aef0a.

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -112,11 +112,12 @@ return {
     "numToStr/Comment.nvim",
     -- stylua: ignore
     keys = {
-      { "<leader>/", function() require("Comment.api").toggle.linewise.current() end,            mode = "n", desc = "[Comment] Comment current line", },
-      { "<leader>/", function() require('Comment.api').toggle.linewise(vim.fn.visualmode()) end, mode = "v", desc = "[Comment] Comment current line", },
+      { "<leader>/", function() require("Comment.api").toggle.linewise.current() end,                 mode = "n", desc = "[Comment] Comment current line", },
+      { "<leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", mode = "v", desc = "Comment current line",           },
       -- control + / keymappings
-      { "<C-_>",     function() require("Comment.api").toggle.linewise.current() end,            mode = "n", desc = "[Comment] Comment current line", },
-      { "<C-_>",     function() require('Comment.api').toggle.linewise(vim.fn.visualmode()) end, mode = "v", desc = "[Comment] Comment current line", },
+      { "<C-_>",     function() require("Comment.api").toggle.linewise.current() end,                 mode = "n", desc = "[Comment] Comment current line", },
+      { "<C-_>",     "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", mode = "v", desc = "Comment current line",           },
+
     },
     config = function()
       require("Comment").setup()
