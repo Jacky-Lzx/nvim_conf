a4dbb29d9336f101fe7a225e51310d92479c8f06
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-03-31 00:36:48 +0800

    feat(overseer): changed the ui of overseer.

--- lua/plugins/code-runner.lua
+++ lua/plugins/code-runner.lua
@@ -18,6 +18,31 @@ return {
         "user.convert_md_to_pdf",
         "user.verilog",
       },
+      task_list = {
+        -- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        -- min_width and max_width can be a single value or a list of mixed integer/float types.
+        -- max_width = {100, 0.2} means "the lesser of 100 columns or 20% of total"
+        max_width = { 100, 0.2 },
+        -- min_width = {40, 0.1} means "the greater of 40 columns or 10% of total"
+        min_width = { 40, 0.1 },
+        -- optionally define an integer/float for the exact width of the task list
+        width = nil,
+        max_height = { 30, 0.4 },
+        min_height = 10,
+        height = nil,
+        -- String that separates tasks
+        separator = "────────────────────────────────────────",
+        -- Default direction. Can be "left", "right", or "bottom"
+        direction = "bottom",
+        -- Set keymap to false to remove default behavior
+        -- You can add custom keymaps here as well (anything vim.keymap.set accepts)
+        bindings = {
+          ["<A-l>"] = "IncreaseDetail",
+          ["<A-h>"] = "DecreaseDetail",
+          ["<A-k>"] = "ScrollOutputUp",
+          ["<A-j>"] = "ScrollOutputDown",
+        },
+      },
     },
     config = function(_, opts)
       require("overseer").setup(opts)
