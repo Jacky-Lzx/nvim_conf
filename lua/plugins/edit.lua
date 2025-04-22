356d727d16dce7bfae90f69bd679db0d17d3186c
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-04-22 13:52:11 +0800

    feat(im-select): disabled im-select.

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -1,20 +1,20 @@
 return {
 
   -- Change input method automatically
-  {
-    "keaising/im-select.nvim",
-    lazy = false,
-    cond = function()
-      return vim.uv.os_uname().sysname == "Darwin"
-    end,
-    opts = {
-      -- Restore the default input method state when the following events are triggered
-      set_default_events = { "VimEnter", "InsertLeave", "CmdlineLeave" },
-    },
-    config = function(_, opts)
-      require("im_select").setup(opts)
-    end,
-  },
+  -- {
+  --   "keaising/im-select.nvim",
+  --   lazy = false,
+  --   cond = function()
+  --     return vim.uv.os_uname().sysname == "Darwin"
+  --   end,
+  --   opts = {
+  --     -- Restore the default input method state when the following events are triggered
+  --     set_default_events = { "VimEnter", "InsertLeave", "CmdlineLeave" },
+  --   },
+  --   config = function(_, opts)
+  --     require("im_select").setup(opts)
+  --   end,
+  -- },
 
   {
     "jake-stewart/multicursor.nvim",
