7c55a5f1e66f14989e445feeaf620aa3e3afd03d
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-05-01 00:09:35 +0800

    feat(autopairs): changed the behavior of autopairs, will not auto add pair if the next char is not empty.

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -101,7 +101,10 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    config = true,
+    opts = {
+      -- ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
+      ignored_next_char = "[^%s]", -- If next char is not an empty char, don't add pairs
+    },
   },
 
   {
