db8881db1616dd267bb000aef00076116b871271
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-05-13 16:05:20 +0800

    feat(multicursor): changed the config of multicursor.

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -15,44 +15,27 @@ return {
   --     require("im_select").setup(opts)
   --   end,
   -- },
-
   {
     "jake-stewart/multicursor.nvim",
     branch = "1.0",
     event = "BufReadPost",
+     -- stylua: ignore
+   keys = {
+    -- Append/insert for each line of visual selections. Similar to block selection insertion.
+    { "mI", function() require("multicursor-nvim").insertVisual() end, mode = "x", desc = "Insert cursors at visual selection" },
+    { "mA", function() require("multicursor-nvim").appendVisual() end, mode = "x", desc = "Append cursors at visual selection" },
+   },
     config = function()
       local mc = require("multicursor-nvim")
       mc.setup()
 
-      local set = vim.keymap.set
-
-      -- Append/insert for each line of visual selections.
-      -- Similar to block selection insertion.
-      set("x", "mI", mc.insertVisual)
-      set("x", "mA", mc.appendVisual)
-
-      -- Mappings defined in a keymap layer only apply when there are
-      -- multiple cursors. This lets you have overlapping mappings.
+      -- Mappings defined in a keymap layer only apply when there are multiple cursors. This lets you have overlapping mappings.
       mc.addKeymapLayer(function(layerSet)
         -- Enable and clear cursors using escape.
         layerSet("n", "<esc>", function()
-          if not mc.cursorsEnabled() then
-            mc.enableCursors()
-          else
-            mc.clearCursors()
-          end
+          mc.clearCursors()
         end)
       end)
-
-      -- Customize how cursors look.
-      local hl = vim.api.nvim_set_hl
-      hl(0, "MultiCursorCursor", { link = "Cursor" })
-      hl(0, "MultiCursorVisual", { link = "Visual" })
-      hl(0, "MultiCursorSign", { link = "SignColumn" })
-      hl(0, "MultiCursorMatchPreview", { link = "Search" })
-      hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
-      hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
-      hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
     end,
   },
 
