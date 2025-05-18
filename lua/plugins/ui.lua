5d4d5955da90a6bc425d2a035630a02f07ee60ff
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-05-18 10:49:05 +0800

    feat(ui): added nvim-tree.lua plugin.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -14,6 +14,18 @@ return {
   },
 
   {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      { "<leader>e", "<CMD>NvimTreeToggle<CR>", mode = { "n" }, desc = "[NvimTree] Toggle NvimTree" },
+    },
+    opts = {},
+  },
+
+  {
     "HiPhish/rainbow-delimiters.nvim",
     event = "BufReadPost",
     submodules = false,
