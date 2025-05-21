11b546e4b0527705847953dba948d8c4e26c4fdc
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-05-21 12:14:36 +0800

    feat(neovide): changed the neovide config.

--- lua/neovide/neovide.lua
+++ lua/neovide/neovide.lua
@@ -1,38 +1,19 @@
--- removed
--- vim.g.neovide_input_macos_alt_is_meta = true
+vim.o.guifont = "SauceCodePro NF:b,LXGW WenKai Mono Medium"
 
--- vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+vim.g.neovide_opacity = 0.7
+vim.g.neovide_normal_opacity = 0.7
 
--- vim.g.transparency = 0.2
+vim.g.neovide_input_macos_option_key_is_meta = "only_left"
 
--- Helper function for transparency formatting
-local alpha = function()
-  return string.format("%x", math.floor(255 * (vim.g.neovide_transparency or 0.8)))
-end
--- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-vim.g.neovide_transparency = 0.8
-vim.g.transparency = 0.2
-vim.g.neovide_background_color = "#282828" .. alpha()
--- Add keybinds to change transparency
+-- -- Add keybinds to change transparency
 local change_transparency = function(delta)
-  vim.g.neovide_transparency = vim.g.neovide_transparency + delta
-  -- vim.g.transparency = vim.g.transparency + delta
-  vim.g.neovide_background_color = "#282828" .. alpha()
+  vim.g.neovide_opacity = math.max(math.min(vim.g.neovide_opacity + delta, 1), 0)
+  vim.g.neovide_normal_opacity = math.max(math.min(vim.g.neovide_normal_opacity + delta, 1), 0)
 end
-vim.keymap.set({ "n", "v", "o" }, "<D-]>", function()
-  change_transparency(0.1)
-end)
-vim.keymap.set({ "n", "v", "o" }, "<D-[>", function()
-  change_transparency(-0.1)
-end)
-
-vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
-vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
-vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+-- stylua: ignore
+vim.keymap.set({ "n", "v", "o" }, "<D-]>", function() change_transparency(0.1) end)
+-- stylua: ignore
+vim.keymap.set({ "n", "v", "o" }, "<D-[>", function() change_transparency(-0.1) end)
 
 vim.g.neovide_scale_factor = 1.0
 local change_scale_factor = function(delta)
@@ -44,8 +25,8 @@ end)
 vim.keymap.set("n", "<D-->", function()
   change_scale_factor(1 / 1.1)
 end)
-
--- Allow clipboard copy paste in neovim
+--
+-- -- Allow clipboard copy paste in neovim
 vim.g.neovide_input_use_logo = 1
 vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
