cbaea3fb207d181288fa77ea0b2f412f402fecd4
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-05-29 09:39:59 +0800

    feat(lsp): updated the lsp configs of markdown.

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -5,14 +5,9 @@ vim.g.markdown_recommended_style = 0
 
 function M.setup(setting_name, extra)
   if setting_name == G.language.lsp then
-    require("lspconfig").marksman.setup({
-      -- on_attach = extra.on_attach,
-      capabilities = extra.capabilities,
-    })
-    require("lspconfig").vale_ls.setup({
-      -- on_attach = extra.on_attach,
-      capabilities = extra.capabilities,
-    })
+    vim.lsp.enable("vale_ls")
+    vim.lsp.enable("marksman")
+
     return
   end
 
