638a2da0bafb01b728295a38b59099e38f9002ab
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-06-03 16:50:03 +0800

    feat(python): added venv-selector.nvim for python.

--- lua/plugins/languages/python.lua
+++ lua/plugins/languages/python.lua
@@ -2,6 +2,22 @@ local M = {}
 
 M.plugins = {
   {
+    "linux-cultist/venv-selector.nvim",
+    branch = "regexp", -- Use this branch for the new version
+    cmd = "VenvSelect",
+    opts = {
+      settings = {
+        options = {
+          notify_user_on_venv_activation = true,
+        },
+      },
+    },
+    --  Call config for python files and load the cached venv automatically
+    ft = "python",
+    keys = { { "<leader>cv", "<CMD>VenvSelect<CR>", desc = "Select VirtualEnv", ft = "python" } },
+  },
+
+  {
     "mason-org/mason.nvim",
     opts = { ensure_installed = { "ruff", "pyright" } },
   },
