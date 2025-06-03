32980bb953751ab023c37960dc1166ebc91ce478
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-06-03 21:35:04 +0800

    feat(git): updated git related configs in gitsigns and snacks.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -127,10 +127,10 @@ return {
       { "<leader>sr", function() require("snacks").picker.recent() end, desc = "[Snacks] Recent", },
       -- git
       { "<C-g>", function() require("snacks").lazygit() end, desc = "[Snacks] Lazygit", },
-      { "<leader>gl", function() require("snacks").picker.git_log() end, desc = "[Snacks] Git log", },
-      { "<leader>gd", function() require("snacks").picker.git_diff() end, desc = "[Snacks] Git diff", },
-      { "<leader>gb", function() require("snacks").git.blame_line() end, desc = "[Snacks] Git blame line", },
-      { "<leader>gB", function() require("snacks").gitbrowse() end, desc = "[Snacks] Git browse", },
+      { "<leader>ggl", function() require("snacks").picker.git_log() end, desc = "[Snacks] Git log", },
+      { "<leader>ggd", function() require("snacks").picker.git_diff() end, desc = "[Snacks] Git diff", },
+      { "<leader>ggb", function() require("snacks").git.blame_line() end, desc = "[Snacks] Git blame line", },
+      { "<leader>ggB", function() require("snacks").gitbrowse() end, desc = "[Snacks] Git browse", },
       -- Grep
       -- { "<leader>sb", function() require("snacks").picker.lines() end, desc = "[Snacks] Buffer lines" },
       -- { "<leader>sB", function() require("snacks").picker.grep_buffers() end, desc = "[Snacks] Grep open buffers" },
@@ -242,7 +242,7 @@ return {
           Snacks.toggle.treesitter():map("<leader>tT")
           Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>tb")
           Snacks.toggle.inlay_hints():map("<leader>th")
-          Snacks.toggle.indent():map("<leader>tg")
+          Snacks.toggle.indent():map("<leader>tG")
           Snacks.toggle.dim():map("<leader>tD")
           -- Toggle the profiler
           Snacks.toggle.profiler():map("<leader>tpp")
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -99,9 +99,76 @@ return {
       signcolumn = false,
       numhl = true,
       -- word_diff = true,
-      -- Toggle the line blame by `:Gitsigns toggle_current_line_blame`
       current_line_blame = true,
       attach_to_untracked = true,
+      on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        -- NOTE: Navigation is handled by mini.diff
+        -- map("n", "]h", function() if vim.wo.diff then vim.cmd.normal({ "]h", bang = true }) else gitsigns.nav_hunk("next") end end, { desc = "[Git] Next hunk" })
+        -- map("n", "[h", function() if vim.wo.diff then vim.cmd.normal({ "[h", bang = true }) else gitsigns.nav_hunk("prev") end end, { desc = "[Git] Prev hunk" })
+
+        -- Actions
+        map("n", "<leader>ggs", gitsigns.stage_hunk, { desc = "[Git] Stage hunk" })
+        -- stylua: ignore
+        map("v", "<leader>ggs", function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, { desc = "[Git] Stage hunk (Visual)" })
+
+        map("n", "<leader>ggr", gitsigns.reset_hunk, { desc = "[Git] Reset hunk" })
+        -- stylua: ignore
+        map("v", "<leader>ggr", function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, { desc = "[Git] Reset hunk (Visual)" })
+
+        map("n", "<leader>ggS", gitsigns.stage_buffer, { desc = "[Git] Stage buffer" })
+        map("n", "<leader>ggR", gitsigns.reset_buffer, { desc = "[Git] Reset buffer" })
+
+        map("n", "<leader>ggp", gitsigns.preview_hunk, { desc = "[Git] Preview hunk" })
+        map("n", "<leader>ggP", gitsigns.preview_hunk_inline, { desc = "[Git] Preview hunk inline" })
+
+        -- map("n", "<leader>ggb", function() gitsigns.blame_line({ full = true }) end, { desc = "[Git] Blame line" })
+
+        -- stylua: ignore
+        map("n", "<leader>ggd", gitsigns.diffthis, { desc = "[Git] diff" })
+        -- stylua: ignore
+        map("n", "<leader>ggD", function() gitsigns.diffthis("~") end, { desc = "[Git] diff (ALL)" })
+
+        -- stylua: ignore
+        map("n", "<leader>ggQ", function() gitsigns.setqflist("all") end, { desc = "[Git] Show diffs (ALL) in qflist" })
+        -- stylua: ignore
+        map("n", "<leader>ggq", gitsigns.setqflist, { desc = "[Git] Show diffs in qflist" })
+
+        -- Text object
+        map({ "o", "x" }, "ih", gitsigns.select_hunk, { desc = "[Git] Current hunk" })
+
+        -- Toggles
+        require("snacks")
+          .toggle({
+            name = "line blame",
+            get = function()
+              return require("gitsigns.config").config.current_line_blame
+            end,
+            set = function(enabled)
+              require("gitsigns").toggle_current_line_blame(enabled)
+            end,
+          })
+          :map("<leader>tgb")
+        require("snacks")
+          .toggle({
+            name = "word diff",
+            get = function()
+              return require("gitsigns.config").config.word_diff
+            end,
+            set = function(enabled)
+              require("gitsigns").toggle_word_diff(enabled)
+            end,
+          })
+          :map("<leader>tgw")
+      end,
     },
     config = function(_, opts)
       require("gitsigns").setup(opts)
@@ -182,12 +249,14 @@ return {
       },
       -- stylua: ignore
       spec = {
-        { "<leader>cc", group = "<CodeCompanion>", icon = "" },
-        { "<leader>D",  group = "<Dap>",           icon = "" },
-        { "<leader>w",  group = "<Workspace>"                   },
-        { "<leader>s",  group = "<Snacks>"                    },
-        { "<leader>t",  group = "<Snacks> Toggle"             },
-        { "<leader>g",  group = "<Trouble> / git"             },
+        { "<leader>tg", group = "[Git] Toggle"                },
+        { "<leader>gg", group = "[Git]"                       },
+        { "<leader>cc", group = "[CodeCompanion]", icon = "" },
+        { "<leader>D",  group = "[Dap]",           icon = "" },
+        { "<leader>w",  group = "[Workspace]"                 },
+        { "<leader>s",  group = "[Snacks]"                    },
+        { "<leader>t",  group = "[Snacks] Toggle"             },
+        { "<leader>g",  group = "[Trouble] / [Git]"           },
       },
       -- expand all nodes wighout a description
       expand = function(node)
