b3e0b18283d5a43812465aa2dfd9433ba50eab8c
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-06-05 15:40:56 +0800

    feat(session): changed back to auto-session from persistence.

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,25 +1,26 @@
 return {
-  -- {
-  --   "rmagatti/auto-session",
-  --   lazy = false,
-  --
-  --   keys = {
-  --     { "<leader>sa", "<CMD>Autosession search<CR>", desc = "[Auto Session] Search session" },
-  --   },
-  --
-  --   ---enables autocomplete for opts
-  --   ---@module "auto-session"
-  --   ---@type AutoSession.Config
-  --   opts = {
-  --     suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-  --   },
-  --
-  --   config = function(_, opts)
-  --     vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-  --
-  --     require("auto-session").setup(opts)
-  --   end,
-  -- },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+
+    keys = {
+      { "<leader>ps", "<CMD>SessionRestore<CR>", desc = "[Auto Session] Restore session" },
+      { "<leader>pS", "<CMD>Autosession search<CR>", desc = "[Auto Session] Search session" },
+      { "<leader>pD", "<CMD>Autosession delete<CR>", desc = "[Auto Session] Delete session" },
+    },
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      auto_restore = false,
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+    },
+
+    init = function()
+      vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+    end,
+  },
 
   -- {
   --   "olimorris/persisted.nvim",
@@ -37,16 +38,19 @@ return {
   -- Session management. This saves your session in the background,
   -- keeping track of open buffers, window arrangement, and more.
   -- You can restore sessions when returning through the dashboard.
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = {},
-    -- stylua: ignore
-    keys = {
-      { "<leader>ps", function() require("persistence").load() end, desc = "Restore Session" },
-      { "<leader>pS", function() require("persistence").select() end,desc = "Select Session" },
-      { "<leader>pl", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-      { "<leader>pd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
-    },
-  },
+  -- {
+  --   "folke/persistence.nvim",
+  --   event = "BufReadPre",
+  --   opts = {},
+  --   -- stylua: ignore
+  --   keys = {
+  --     { "<leader>ps", function() require("persistence").load() end, desc = "Restore Session" },
+  --     { "<leader>pS", function() require("persistence").select() end,desc = "Select Session" },
+  --     { "<leader>pl", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+  --     { "<leader>pd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+  --   },
+  --   init = function()
+  --     vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+  --   end,
+  -- },
 }
