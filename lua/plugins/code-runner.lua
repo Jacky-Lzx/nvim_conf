b42b4e0e46e7fd23ebb6a7fb2a6d2f8ec70145ea
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-08-28 21:09:52 +0800

    feat(overseer): changed the window options of overseer.

--- lua/plugins/code-runner.lua
+++ lua/plugins/code-runner.lua
@@ -35,9 +35,9 @@ return {
         -- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
         -- min_width and max_width can be a single value or a list of mixed integer/float types.
         -- max_width = {100, 0.2} means "the lesser of 100 columns or 20% of total"
-        max_width = { 100, 0.2 },
+        max_width = { 100, 0.6 },
         -- min_width = {40, 0.1} means "the greater of 40 columns or 10% of total"
-        min_width = { 40, 0.1 },
+        min_width = { 60, 0.4 },
         -- optionally define an integer/float for the exact width of the task list
         width = nil,
         max_height = { 30, 0.4 },
