e158f1129a055e8c8ada7976c0223fed4d44487a
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-08-29 11:13:47 +0800

    feat(mini.bracketed): changed the keymappings of indent for mini.bracketed.

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -51,7 +51,44 @@ return {
     "echasnovski/mini.bracketed",
     version = "*",
     event = "BufReadPost",
-    config = true,
+    keys = {
+      {
+        "]i",
+        function()
+          require("mini.bracketed").indent("forward", { change_type = "more" })
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Inner indent forward",
+      },
+      {
+        "[i",
+        function()
+          require("mini.bracketed").indent("backward", { change_type = "more" })
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Inner indent backward",
+      },
+      {
+        "]o",
+        function()
+          require("mini.bracketed").indent("forward", { change_type = "less" })
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Outer indent forward",
+      },
+      {
+        "[o",
+        function()
+          require("mini.bracketed").indent("backward", { change_type = "less" })
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Outer indent backward",
+      },
+    },
+    opts = {
+      indent = { suffix = "" },
+      oldfile = { suffix = "" },
+    },
   },
 
   {
